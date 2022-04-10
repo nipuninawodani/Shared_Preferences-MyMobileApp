a52ca984197baa6cc3cb72337991fb5993327e15
nipuninawodani <nipuninawodani999@gmail.com>
2022-04-10 15:54:28 +0530

    Bugs Fixed

--- app/src/main/java/com/nnperera/android/vaccination_status/MainActivity.kt
+++ app/src/main/java/com/nnperera/android/vaccination_status/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity()
 {
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity()
         //handle save button click: input data and save in shared preferences
         saveBtn.setOnClickListener {
             //input data
-            val name = nameEt.text.toString().trim()
-            val age = Integer.parseInt(ageEt.text.toString().trim())
+            val name = nameEt.text.toString()
+            val age = Integer.parseInt(ageEt.text.toString())
             val vaccinated = switchh.isChecked //if checked then true else false
 
             //Edit shared preferences to put data
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity()
             //put data in shared preferences
             editor.putString("NAME", name.toString())
             editor.putInt("AGE",age)
-            editor.putBoolean("VACCINATED",vaccinated)
+            editor.putBoolean("vaccinated",vaccinated)
 
             //apply changes to shared preferences
             editor.apply()
@@ -38,12 +38,12 @@ class MainActivity : AppCompatActivity()
         //handle show info button click : get info from shared preferences and show in textview
         showInfoBtn.setOnClickListener {
             //get data from shared preferences
-            val name = sharedPreferences.getString("Name","")
+            val name = sharedPreferences.getString("NAME","")
             val age = sharedPreferences.getInt("AGE",0)
             val vaccinated = sharedPreferences.getBoolean("vaccinated",false)
 
             //show data in text view
-            infoTv.text = "NAME: $name \nAGE: $age \nvaccinated ?: $vaccinated"
+            infoTv.text = "NAME: $name \nAGE: $age \n FULLY VACCINATED: $vaccinated"
         }
 
     }
